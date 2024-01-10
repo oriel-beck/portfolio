75358f56e9e145e14d47a133acdbda4835ef51fb
oriel-beck <orielbeck@gmail.com>
2024-01-10 17:47:03 +0200

    more styling for contact

--- app/_components/submit.tsx
+++ app/_components/submit.tsx
@@ -1,7 +1,8 @@
 import { useFormStatus } from "react-dom";
 import Send from "./icons/send";
+import colors from '../colors.module.scss';
 
 export default function Submit() {
     const status = useFormStatus();
-    return <button disabled={status.pending} className=" disabled:opacity-70 flex flex-row items-center justify-center font-bold p-2 rounded-lg w-24 bg-green-400 space-x-2"><span>{status.pending ? 'Sending' : 'Send'}</span> <i aria-hidden='true'><Send height={25} width={25} /></i></button>
+    return <button disabled={status.pending} className="disabled:opacity-70 flex flex-row items-center justify-center font-bold p-2 rounded-lg space-x-2 text-white" style={{ backgroundColor: colors.primaryColor }}><span>{status.pending ? 'Sending' : 'Send'}</span> <i aria-hidden='true'><Send height={25} width={25} /></i></button>
 }
