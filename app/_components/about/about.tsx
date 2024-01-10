b8dae8b21fe74a639c461bb6f66b121ae3dd4573
oriel-beck <orielbeck@gmail.com>
2024-01-10 12:48:11 +0200

    break down the components

--- app/_components/about/about.tsx
+++ app/_components/about/about.tsx
@@ -1,4 +1,4 @@
-import colors from '../../colors.module.scss'
+import Heading from '../heading/heading'
 import Chip from './chip'
 
 export default function About() {
@@ -6,15 +6,22 @@ export default function About() {
     return (
         <div className="flex md:flex-row sm:flex-col items-center justify-center p-10 space-x-10 h-full">
             <div className="basis-1/2 flex flex-col">
-                <h2 className="text-2xl font-bold pb-5" style={{ color: colors.primaryColor }}>About me</h2>
+                <div className='pb-5'>
+                    <Heading heading='About me' level='h2' />
+                </div>
+                {/* <h2 className="text-2xl font-bold pb-5" style={{ color: colors.primaryColor }}>About me</h2> */}
                 <p className='text-white'>Hi, I&apos;m Oriel Beck, a Full Stack developer with a passion for creating web applications that are fast, functional, and user-friendly. I have experience in working with Angular, React, and Vue frameworks, as well as using Agile methodology and TDD principles. I have worked as a Full Stack developer for Zoominfo, a leading enterprise software company, and Winfluence, a startup that provides email campaigns management solutions. I have also developed nekos.best, an anime gif and image API that serves hundreds of thousands of requests and visitors every month. I am always eager to learn new technologies and improve my skills as a developer. I am looking for opportunities to work on challenging and innovative projects that can make a positive impact on the world.</p>
             </div>
             <div className="basis-1/2 flex flex-col">
-                <h2 className="text-2xl font-bold pb-5 ml-2" style={{ color: colors.primaryColor }}>My skills</h2>
+                <div className="pb-5">
+
+                    <Heading heading='My skills' level='h2' />
+                </div>
+                {/* <h2 className="text-2xl font-bold pb-5 ml-2" style={{ color: colors.primaryColor }}>My skills</h2> */}
                 <ul className="flex items-center flex-row flex-wrap">
                     {skills.map((skill, i) => (
                         <li key={i} className="p-2 mr-1 mb-2">
-                            <Chip label={skill}/>
+                            <Chip label={skill} />
                         </li>
                     ))}
                 </ul>
---
+++ app/_components/heading/heading.tsx
@@ -0,0 +1,18 @@
+import colors from '../../colors.module.scss';
+
+export default function Heading({ heading, level }: { heading: string, level: 'h1' | 'h2' | 'h3' | 'h4' | 'h5' | 'h6' }) {
+    switch(level) {
+        case "h1":
+            return <h1 className='text-2xl font-bold' style={{ color: colors.primaryColor }}>{heading}</h1>
+        case "h2":
+            return <h2 className='text-2xl font-bold' style={{ color: colors.primaryColor }}>{heading}</h2>
+        case "h3":
+            return <h3 className='text-2xl font-bold' style={{ color: colors.primaryColor }}>{heading}</h3>
+        case "h4":
+            return <h4 className='text-2xl font-bold' style={{ color: colors.primaryColor }}>{heading}</h4>
+        case "h5":
+            return <h5 className='text-2xl font-bold' style={{ color: colors.primaryColor }}>{heading}</h5>
+        case "h6":
+            return <h6 className='text-2xl font-bold' style={{ color: colors.primaryColor }}>{heading}</h6>
+    }
+}
