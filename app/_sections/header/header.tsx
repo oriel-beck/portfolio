deaf5b77d2bf332dbccd544962e736be4720400c
oriel-beck <orielbeck@gmail.com>
2024-01-13 19:09:07 +0200

    fix spacing for very small screen sizes

--- app/_sections/header/header.tsx
+++ app/_sections/header/header.tsx
@@ -5,8 +5,8 @@ import './header.scss';
 export default function Header() {
     return (
         <header className="flex w-full flex-row fixed top-0 right-0" style={{ height: '70px', backgroundColor: 'rgba(40, 40, 40, 0.70)' }}>
-            <nav role='navigation' className='flex flex-row items-center ml-12'>
-                <ul className='flex flex-row justify-center space-x-7 text-white'>
+            <nav role='navigation' className='flex flex-row items-center ml-5 md:ml-12'>
+                <ul className='flex flex-row justify-center space-x-5 md:space-x-7 text-white'>
                     <li><a className='link' href="#about">About</a></li>
                     <li><a className='link' href="#projects">Projects</a></li>
                     <li><a className='link' href="#contact">Contact</a></li>
