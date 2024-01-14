7d76584ae676896a96d2b62d8b612f2b236de91e
oriel-beck <orielbeck@gmail.com>
2024-01-14 10:42:12 +0200

    highlight active link according to the current section

--- app/_sections/header/header.tsx
+++ app/_sections/header/header.tsx
@@ -2,14 +2,14 @@ import Github from '@/app/_components/icons/github';
 import Linkdin from '@/app/_components/icons/linkdin';
 import './header.scss';
 
-export default function Header() {
+export default function Header({ activeSection }: { activeSection?: string | null }) {
     return (
         <header className="flex w-full flex-row fixed top-0 right-0" style={{ height: '70px', backgroundColor: 'rgba(40, 40, 40, 0.70)' }}>
             <nav role='navigation' className='flex flex-row items-center ml-5 md:ml-12'>
                 <ul className='flex flex-row justify-center space-x-5 md:space-x-7 text-white'>
-                    <li><a className='link' href="#about">About</a></li>
-                    <li><a className='link' href="#projects">Projects</a></li>
-                    <li><a className='link' href="#contact">Contact</a></li>
+                    <li><a className={activeSection === "about" ? 'link active' : 'link'} href="#about">About</a></li>
+                    <li><a className={activeSection === "projects" ? 'link active' : 'link'} href="#projects">Projects</a></li>
+                    <li><a className={activeSection === "contact" ? 'link active' : 'link'} href="#contact">Contact</a></li>
                 </ul>
             </nav>
             <span aria-hidden="true" className='flex flex-1'></span>
--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,5 @@
 "use client"
+import { useEffect, useRef, useState } from "react";
 import About from "./_sections/about/about";
 import Contact from "./_sections/contact/contact";
 import Header from "./_sections/header/header";
@@ -7,20 +8,52 @@ import Projects from "./_sections/projects/projects";
 import colors from './colors.module.scss';
 
 export default function Home() {
+  const aboutSectionRef = useRef<HTMLElement | null>();
+  const projectsSectionRef = useRef<HTMLElement | null>();
+  const contactSectionRef = useRef<HTMLElement | null>();
+  const [activeSection, setActiveSection] = useState<string | null>();
+
+  const handleScroll = () => {
+    const pageYOffset = window.scrollY;
+    let newActiveSection = null;
+
+    [aboutSectionRef.current, projectsSectionRef.current, contactSectionRef.current].forEach((section) => {
+      if (section) {
+        const sectionOffsetTop = section.offsetTop;
+        const sectionHeight = section.offsetHeight;
+
+        if (pageYOffset >= sectionOffsetTop && pageYOffset < sectionOffsetTop + sectionHeight) {
+          newActiveSection = section.id;
+        }
+      }
+    });
+
+    setActiveSection(newActiveSection);
+  };
+
+  useEffect(() => {
+    console.log("assigning scroll")
+    window.addEventListener('scroll', handleScroll);
+
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
+
   return (
     <>
-      <Header />
+      <Header activeSection={activeSection} />
       <main className="flex flex-col">
         <section className="h-screen" id="hero" style={{ backgroundColor: colors.bg1 }}>
           <Hero />
         </section>
-        <section className="h-full md:h-screen" id="about" style={{ backgroundColor: colors.bg2 }}>
+        <section ref={(el) => { aboutSectionRef.current = el }} className="h-full md:h-screen" id="about" style={{ backgroundColor: colors.bg2 }}>
           <About />
         </section>
-        <section className="h-full md:h-screen" id="projects" style={{ backgroundColor: colors.bg1 }}>
+        <section ref={(el) => { projectsSectionRef.current = el }} className="h-full md:h-screen" id="projects" style={{ backgroundColor: colors.bg1 }}>
           <Projects />
         </section>
-        <section className="h-full md:h-screen pb-10 md:pb-0" id="contact" style={{ backgroundColor: colors.bg2 }}>
+        <section ref={(el) => { contactSectionRef.current = el }} className="h-full md:h-screen pb-10 md:pb-0" id="contact" style={{ backgroundColor: colors.bg2 }}>
           <Contact />
         </section>
       </main>
